7aca04acbb7037e7db245fa5b2733e24d6f795a5
BernardZgzDev <bernard.moralesn@pandadev.es>
2021-06-13 16:03:22 +0200

    Fix masonry layout

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-const spanYear = document.querySelector('span#year-copyright');
-const now  = new Date();
-const year = now.getFullYear();
 
-spanYear.innerText = year;
+
+$(document).ready(function() {
+
+      const spanYear = document.querySelector('span#year-copyright');
+      const now  = new Date();
+      const year = now.getFullYear();
+      spanYear.innerText = year;
+
+      
+      $('.gallery-masonry').imagesLoaded( function() {
+            $('.gallery-masonry').masonry({
+                  itemSelector: '.card',
+                  isAnimated: true,
+                  isFitWidth: false
+            });
+      });
+});
